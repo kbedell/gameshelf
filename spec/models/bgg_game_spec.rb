49514dd6f0e556e99c9be9fe7a327244c4d29870
Kat Bedell <kattbedell@gmail.com>
2016-07-22 14:08:50 -0400

    implemented api tests

--- spec/models/bgg_game_spec.rb
+++ spec/models/bgg_game_spec.rb
@@ -1,1 +1,50 @@
 require 'rails_helper'
+
+describe '#create_games_list', vcr: { cassette_name: 'game_search' } do
+  it 'returns an array of games' do
+    game_data = BoardGameGeek.search_games('tokaido')
+    games = BGGGame.create_games_list(game_data)
+
+    expect(games.class).to eq Array
+    expect(games.length).to eq 8
+    expect(games[0][:name]).to eq 'Tokaido'
+  end
+end
+
+describe '#create_game', vcr: { cassette_name: 'game_add' } do
+
+  let!(:user) { FactoryGirl.create(:user) }
+
+  it 'game is nil' do
+    game_data = BoardGameGeek.add_game('123540')
+
+    expect(Game.all.count).to eq 0
+    expect(Usersgame.all.count).to eq 0
+
+    response = BGGGame.create_game('123540', game_data, user.id)
+
+    expect(response.class).to eq Hash
+
+    expect(response[:game][:name]).to eq 'Tokaido'
+    expect(response[:game][:year]).to eq '2012'
+    expect(response[:game][:id]).to eq 1
+    expect(Game.all.count).to eq 1
+    expect(Usersgame.all.count).to eq 1
+  end
+
+  it 'game is not nil' do
+    game = FactoryGirl.create(:game, bggid: 123540)
+
+    expect(Game.all.count).to eq 1
+    expect(Usersgame.all.count).to eq 0
+
+    game_data = BoardGameGeek.add_game('123540')
+    response = BGGGame.create_game('123540', game_data, user.id)
+
+    expect(response[:game][:name]).to eq 'game of thrones19'
+    expect(response[:game][:year]).to eq 2012
+    expect(response[:game][:id]).to eq 2
+    expect(Game.all.count).to eq 1
+    expect(Usersgame.all.count).to eq 1
+  end
+end
--- spec/models/board_game_geek_spec.rb
+++ spec/models/board_game_geek_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe '#search_games', vcr: { cassette_name: 'game_search' } do
   it 'returns Nokogiri XML Document' do
     game_data = BoardGameGeek.search_games('tokaido')
-    binding.pry
 
     expect(game_data.class).to eq Nokogiri::XML::Document
-    expect(hipster_data.code).to eq 200
 
-    expect(hipster_data["text"]).to be_a String
-    expect(game_data).to include "Listicle VHS meggings placeat occaecat"
+    expect(game_data.css('boardgame').class).to eq Nokogiri::XML::NodeSet
+    expect(game_data.css('boardgame').length).to eq 8
   end
 end
 
 describe '#add_game', vcr: { cassette_name: 'game_add' } do
   it 'returns Nokogiri XML Document' do
-    game_data = BoardGameGeek.search_games('tokaido')
+    game_data = BoardGameGeek.add_game('123540')
 
     expect(game_data.class).to eq Nokogiri::XML::Document
+
+    expect(game_data.css("name[primary='true']").text).to eq 'Tokaido'
   end
 end
